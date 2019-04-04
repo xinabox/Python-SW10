87587c7c509c66ba2110f0521543e693e1ec063f
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-04-05 00:56:25 +0200

    Update SW10.py

--- SW10.py
+++ SW10.py
@@ -1,8 +1,15 @@
 from microbit import i2c
 
+SW10_ADDR = 0x4B
+LM75B_REG_TEMP = 0x00
+
 class SW10:
-	def readTempC():
-    	i2c.write(SW10_ADDR, str(LM75B_REG_TEMP))
-    	data = i2c.read(SW10_ADDR, 2)
-    	tempC = (data[0] * 256 + data[1])/32 *0.125
-    	return tempC
+    
+    def __init__(self, addr=SW10_ADDR):
+        self.addr = addr
+        
+    def readTempC(self):
+        i2c.write(self.addr, str(LM75B_REG_TEMP))
+        data = i2c.read(self.addr, 2)
+        tempC = (data[0] * 256 + data[1])/32 * 0.125
+        return tempC
