08772d0a84cbdb710da3b1ec57ae4451b6f6055c
☒ Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2020-05-03 09:20:55 +0200

    Rename from _AANN to xAANN

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,9 @@
 from microbit import *
-from SW10 import SW10
+from xSW10 import xSW10
 
-SW10 = SW10()
+xSW10 = xSW10()
 
 while True:
-    temp = SW10.readTempC()
+    temp = xSW10.readTempC()
     display.scroll(temp)
     sleep(1000)
