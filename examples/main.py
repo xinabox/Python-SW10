6fc816686b298d0f40ee6814d34953b2c70ed3a8
☒ Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2020-05-03 09:22:29 +0200

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,9 @@
 from microbit import *
 from xSW10 import xSW10
 
-xSW10 = xSW10()
+SW10 = xSW10()
 
 while True:
-    temp = xSW10.readTempC()
+    temp = SW10.readTempC()
     display.scroll(temp)
     sleep(1000)
